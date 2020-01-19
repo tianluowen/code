e5817f74127f4d6c82ed0fc2d9fec80b26236e18
tianluowen <1308145492@qq.com>
2020-01-19 23:21:35 +0800

    2019-01-19

--- book/cppPrimerPlus/chapter_07/02/main.cpp
+++ book/cppPrimerPlus/chapter_07/02/main.cpp
@@ -5,6 +5,22 @@
 
 int main(int argc, char *argv[])
 {
+    float sorce[10];
+    int i = 0;
+
+    std::cout << "请依次输入 10 个高尔夫球成绩\n";
+    do {
+        std::cout << "请输入第 " << i + 1 << " 次成绩: ";
+        std::cin >> sorce[i];
+        i++;
+    }
+    while (i < 10);
+
+    for (int j = 0; j < i; j++)
+    {
+        std::cout << sorce[j] << " ";
+    }
+    std::cout << std::endl;
     
     return 0;
 }
