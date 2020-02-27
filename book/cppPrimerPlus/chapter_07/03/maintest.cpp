79db60245dfa16bf2dea7cf1cd56df549075103a
tianluowen <1308145492@qq.com>
2020-02-27 13:58:29 +0800

    2020-02-27 update

---
+++ book/cppPrimerPlus/chapter_07/03/maintest.cpp
@@ -0,0 +1,63 @@
+// date: 2020-02-27
+// desc: 模仿一个类编写函数操作结构体
+
+#include <iostream>
+#include <string.h>
+
+struct boxinfo
+{
+    char maker[40];
+    float height;
+    float width;
+    float length;
+    float volume;
+};
+
+class box
+{
+private:
+    boxinfo cuboidbox;
+
+public:
+    box(char* str, float height, float width, float length) 
+    {
+        strcpy(cuboidbox.maker, str);
+        cuboidbox.height = height;
+        cuboidbox.width = width;
+        cuboidbox.length = length;
+    }
+    void printStructBox(void)
+    {
+        std::cout << "cuboidbox.maker is " << cuboidbox.maker << "\ncuboidbox.height = " << cuboidbox.height << "\ncuboidbox.width = " << cuboidbox.width 
+            << "\ncuboidbox.length = " << cuboidbox.length << "\ncuboidbox.volume = " << cuboidbox.volume << std::endl;
+    }
+
+    void setStructBoxMemberVolume(void)
+    {
+        cuboidbox.volume = cuboidbox.height * cuboidbox.width * cuboidbox.length;
+    }
+};
+
+
+/* void printStructBox(box st); */
+/* void setStructBoxMemberVolume(box* pst); */
+
+int main(int argc, char *argv[])
+{
+    box *cuboid = new box("长方体", 3.13, 2.15, 3.48);
+
+    cuboid->printStructBox();
+    cuboid->setStructBoxMemberVolume();
+    cuboid->printStructBox();
+
+    /* printStructBox(boxinfo); */
+    /* setStructBoxMemberVolume(&boxinfo); */
+    /* std::cout << std::endl; */
+    /* printStructBox(boxinfo); */
+
+    return 0;
+}
+
+
+
+
---
+++ book/cppPrimerPlus/chapter_07/04/main.cpp
@@ -0,0 +1,10 @@
+// date: 2020-02-27 
+// desc: 计算彩票中奖的概率
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
