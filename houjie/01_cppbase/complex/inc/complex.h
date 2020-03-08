dce5b51d0540115711b07cfb57f580a7944b6c34
tianluowen <1308145492@qq.com>
2020-03-08 22:22:23 +0800

    update 2020-03-08

--- houjie/01_cppbase/complex/inc/complex.h
+++ houjie/01_cppbase/complex/inc/complex.h
@@ -4,53 +4,202 @@
 #ifndef __complex__ 
 #define __complex__ 
 
+using namespace std;
+
 // 声明类
 // 使用模板函数模板类
-
-template<typename T>
-
+namespace testComplex
+{
+// template<typename T>
 class complex
 {
 private:
-    T re, im;
+    double re, im;
+
+    friend complex& __doapl(complex* ths, const complex& r); 
+    friend complex& __doapl(complex* ths, const double& x);
+    friend complex& __dosub(complex* ths, const complex& x);
+    friend complex& __dosub(complex* ths, const double& x);
 
 public:
-    complex (T r = 0, T i = 0)
-        : re (r), im (i)
+    complex(double r = 0, double i = 0)
+        : re(r), im(i)
     { }
 
     // 获取值，和设置值
-    T real(void) const
+    double real(void) const
     {
         return re;
     }
-    T imag(void) const
+    double imag(void) const
     {
         return im;
     }
-    
-    void real(T r) 
+    void real(double r) 
     {
         re = r;
     }
-    void imag(T i)
+    void imag(double i)
     {
         im = i;
     }
 
     // 操作符重载
-    
     complex& operator += (const complex& x);
-    
-    // 共轭
+    complex& operator += (const double& x);
+    complex& operator -= (const complex& x);
+    complex& operator -= (const double& x);
 
-    // 输出打印
+    // 等或者不相等
+
+    // 共轭
+    complex Conjugate(void);
+
+};
+
+// += 重载
+inline complex& complex::operator += (const complex& r)
+{
+    return __doapl(this, r);
 }
 
-
-complex& complex::operator += (const complex& x)
+inline complex& __doapl(complex* ths, const complex& r)
 {
-    return complex(this->re + x->re, this->im + x->im);
+    ths->re += r.re;
+    ths->im += r.im;
+    // 当函数不被声明为 frined 的时候，函数可以通过下面的方式使用
+    // ths->real(ths->real() + r.real());
+    // ths->imag(ths->imag() + r.imag());
+
+    return *ths;
+}
+
+inline complex& complex::operator += (const double& x)
+{
+    return __doapl(this, x);
+}
+
+inline complex& __doapl(complex* ths, const double& x)
+{
+    ths-> re += x;
+
+    return *ths;
+}
+
+// -= 重载
+inline complex& complex::operator -= (const complex& x)
+{
+    return __dosub(this, x);
+}
+
+inline complex& __dosub(complex* ths, const complex& x)
+{
+    ths->re -= x.re;
+    ths->im -= x.im;
+
+    return *ths;
+}
+
+inline complex& complex::operator -= (const double& x)
+{
+    return __dosub(this, x);
+}
+
+inline complex& __dosub(complex* ths, const double& x)
+{
+    ths->re -= x;
+
+    return *ths;
+}
+
+// 输出格式
+ostream& operator << (ostream& os, const complex& r)
+{
+    return os << '(' << r.real() << ", " << r.imag() << ')'; 
+}
+
+// + 重载 
+inline complex operator + (const complex& x, const complex& y)
+{
+    return complex(x.real() + y.real(), x.imag() + y.imag());
+}
+
+inline complex operator + (const complex& x, const double& y)
+{
+    return complex(x.real() + y, x.imag());
+}
+
+inline complex operator + (const double& x, const complex& y)
+{
+    return complex(x + y.real(), y.imag());
+}
+
+// - 重载
+inline complex operator - (const complex& x, const complex& y)
+{
+    return complex(x.real() - y.real(), x.imag() - y.imag());
+}
+
+inline complex operator - (const complex& x, const double& y)
+{
+    return complex(x.real() - y, x.imag());
+}
+
+inline complex operator - (const double& x, const complex& y)
+{
+    return complex(x - y.real(), y.imag());
+}
+
+// * 重载
+inline complex operator * (const complex& x, const complex& y)
+{
+    return complex(x.real() * y.real() - x.imag() * y.imag(),
+            x.imag() * y.real() + x.real() + y.imag());
+}
+
+inline complex operator * (const complex& x, const double& y)
+{
+    return complex(x.real() * y, x.imag() * y);
+}
+
+inline complex operator * (const double& x, const complex& y)
+{
+    return complex(x * y.real(), x * y.imag());
+}
+
+// \ 重载
+inline complex operator / (const complex& x, const complex& y)
+{
+    float a = x.real();
+    float b = x.imag();
+    float c = y.real();
+    float d = y.imag();
+
+    return complex((a * c + b * d) / (c * c + d * d),
+                   (b * c - a * d) / (c * c + d * d));
+}
+
+inline complex operator / (const complex& x, const double& y)
+{
+    return complex(x.real() / y, x.imag() / y);
+}
+
+inline complex operator / (const double& x, const complex& y)
+{
+    float a = x;
+    float c = y.real();
+    float d = y.imag();
+
+    return complex((a * c) / (c * c + d * d),
+                   (0 - a * d) / (c * c + d * d));
+}
+
+// 共轭
+inline complex Conjugate(void)
+{
+    return complex(this->re, -this->im);
+}
+
 }
 
 
--- houjie/01_cppbase/complex/src/main.cpp
+++ houjie/01_cppbase/complex/src/main.cpp
@@ -1,2 +1,94 @@
-// date: 2020-03-07
-// desc: 测试 complex.h
+// date: 2020-03-08
+// desc: 测试 complex.h 
+
+#include <iostream>
+#include "../inc/complex.h"
+
+using namespace testComplex;
+
+int main(void)
+{
+    // 测试创建类对象
+    complex c1(2, 1);               // 栈中分配 这种方式参数不可以为空
+    complex c2 = complex(2, 2);     // 栈中分配
+    complex* pc3 = new complex(4);  // 堆中分配
+    complex c4 = complex();
+    complex c5(1);
+    // complex c6();
+    complex c7;
+
+    // 测试输出对象 << 操作符重载效果
+    std::cout << "// test 创建类对象 & <<" << std::endl;
+    std::cout << "complex c1(2, 1)              c1 = " << c1 << std::endl;
+    std::cout << "complex c2 = complex(2, 2)    c2 = " << c2 << std::endl;
+    std::cout << "complex* c3 = new complex(4)  c3 = " << *pc3 << std::endl;
+    std::cout << "complex c4 = complex()        c4 = " << c4 << std::endl;
+    std::cout << "complex c5(1)                 c5 = " << c5 << std::endl;
+    // std::cout << "complex c6()                  c6 = " << c6 << std::endl;
+    std::cout << "complex c7                    c7 = " << c7 << std::endl;
+
+    // test +=
+    std::cout << std::endl << "// test +=" << std::endl;
+    std::cout << "c1 = " << c1 << ", c2 = " << c2 << std::endl;
+    c1 += c2;
+    std::cout << "c1 += c2, c1 = " << c1 << std::endl;
+    std::cout << "c3 = " << *pc3 << ", c1 = " << c1 << std::endl;
+    *pc3 += c1;
+    std::cout << "c3 += c1, c3 = " << *pc3 << std::endl;
+    std::cout << "c1 = " << c1 << std::endl;
+    c1 += 10;
+    std::cout << "c1 += 10, c1 = " << c1 << std::endl;
+    std::cout << "c1 = " << c1 << ", c5 = " << c5 << std::endl;
+    c5 += c1 += 10;
+    std::cout << "c5 += c1 += 10, c5 = " << c5 << " , c1 = " << c1 << std::endl;
+
+    // test -=
+    std::cout << std::endl << "// test -=" << std::endl;
+    std::cout << "c1 = " << c1 << ", c2 = " << c2 << std::endl;
+    c1 -= c2;
+    std::cout << "c1 -= c2, c1 = " << c1 << std::endl;
+    std::cout << "c3 = " << *pc3 << ", c1 = " << c1 << std::endl;
+    *pc3 -= c1;
+    std::cout << "c3 -= c1, c3 = " << *pc3 << std::endl;
+    std::cout << "c1 = " << c1 << std::endl;
+    c1 -= 10;
+    std::cout << "c1 -= 10, c1 = " << c1 << std::endl;
+    std::cout << "c5 = " << c5 << ", c1 = " << c1 << std::endl;
+    c5 -= c1 -= 10;
+    std::cout << "c5 -= c1 -= 10, c5 = " << c5 << " , c1 = " << c1 << std::endl;
+
+    // test +
+    std::cout << std::endl << "// test +" << std::endl;
+    c4 = c1 + c2;
+    std::cout << "c4 = c1 + c2, c1 = " << c1 << ", c2 = " << c2 << ", c4 =" << c4 << std::endl;
+    c4 = c4 + 7;
+    std::cout << "c4 = c4 + 7, c4 = " << c4 << std::endl;
+    c5 = 5 + c4;
+    std::cout << "c5 = 5 + c4, c5 = " << c5 << std::endl;
+    c5 = 7 + c5 + 6;
+    std::cout << "c5 = 7 + c5 + 6, c5 = " << c5 << std::endl;
+
+    // test /
+    std::cout << std::endl << "// test /" << std::endl;
+    c5 = c5 / 2;
+    std::cout << "c5 = c5 / 2, c5 = " << c5 << std::endl;
+    c5 = c5 / c4;
+    std::cout << "c5 = c5 / c4, c4 = " << c4 << ", c5 = " << c5 << std::endl;
+    c1 = 3 / c2;
+    std::cout << "c1 = 3 / c2, c2 = " << c2 << ", c1 = " << c1 << std::endl;
+
+    // test *
+    std::cout << std::endl << "// test *" << std::endl;
+    c1 = c1 * c2;
+    std::cout << "c1 = c1 * c2, c2 = " << c2 << ", c1 = " << c1 << std::endl;
+    c5 = c5 * c4;
+    std::cout << "c5 = c5 * c4, c4 = " << c4 << ", c5 = " << c5 << std::endl;
+    c5 = 3 * c5 * c4 * 2;
+    std::cout << "c5 = 3 * c5 * c4 * 2, c5 = " << c5 << std::endl;
+    
+
+    
+    delete pc3;
+
+    return 0;
+}
