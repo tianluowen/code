b033b3129c0a9774ea2263be252ce654dbca4382
tianluowen <1308145492@qq.com>
2020-03-10 23:01:09 +0800

    2020-03-10 update

--- houjie/01_cppbase/mystring/inc/mystring.h
+++ houjie/01_cppbase/mystring/inc/mystring.h
@@ -4,4 +4,21 @@
 #ifndef __MYSTRING__
 #define __MYSTRING__
 
+class String
+{
+private:
+	char* m_data;
+public:
+	String(const char* cstr == 0);
+	String(const String& str);
+	String& operator = (const String& str);
+	~String();
+
+};
+
+~String()
+{
+	delete m_data;
+}
+
 #endif
