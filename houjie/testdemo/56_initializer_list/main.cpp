216dc9ba2df83b55b90d300c721f29d1b1651585
tianluowen <1308145492@qq.com>
2020-04-13 22:58:46 +0800

    2020-04-13 update

---
+++ houjie/testdemo/56_initializer_list/main.cpp
@@ -0,0 +1,33 @@
+// date: 2020-04-13
+// desc: initializer_list<>
+
+#include <iostream>
+
+using namespace std;
+
+class P
+{
+public:
+    P(int a, int b)
+    {
+        cout << "P(int a, int bn), a = " << a << ", b = " << b << endl;
+    }
+
+    P(initializer_list<int> initlist)
+    {
+        cout << "P(initializer_list<int> initlist), values = ";
+        for (auto i : initlist)
+            cout << i << " ";
+        cout << endl;
+    }
+};
+
+int main(void)
+{
+    P p(77, 5);         // P(int a, int bn), a = 77, b = 5
+    P q{77, 5};         // P(initializer_list<int> initlist), values = 77 5 
+    P r{77, 5, 42, 4};     // P(initializer_list<int> initlist), values = 77 5 42
+    P s = {77, 5};      // P(initializer_list<int> initlist), values = 77 5
+
+    return 0;
+}
